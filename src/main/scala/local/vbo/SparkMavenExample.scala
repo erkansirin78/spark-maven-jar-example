32926288cf11b0df261ae870c55e16faa2fc3a53
train centos7 <train@vbo.local>
2021-10-30 19:50:17 +0300

    download file code added

--- src/main/scala/local/vbo/SparkMavenExample.scala
+++ src/main/scala/local/vbo/SparkMavenExample.scala
@@ -1,24 +1,39 @@
 package local.vbo
-
-import org.apache.spark.sql.{DataFrame, SparkSession, functions => F}
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.{SparkSession, functions => F}
+import sys.process._
+import java.net.URL
+import java.io.File
 import org.apache.log4j.{Level, Logger}
 
 object SparkMavenExample extends App{
-  println("Hello Spark Maven!")
+  // Set log level
   Logger.getLogger("org").setLevel(Level.INFO)
 
+  // Create Spark Session
   val spark = SparkSession.builder()
     .appName("Spark Maven Example")
     .master("local[2]")
     .getOrCreate()
 
+  // The function for downloading the Advertising.csv file
+  def fileDownloader(url: String, filename: String) = {
+    new URL(url) #> new File(filename) !!
+  }
+  // Download the file
+  fileDownloader("https://raw.githubusercontent.com/erkansirin78/datasets/master/Advertising.csv",
+    "/tmp/Advertising.csv")
+
+  // Read the file
   val df = spark.read.format("csv")
     .option("header", "true")
     .option("inferSchema", "true")
     .option("sep",",")
-    .load("file:///home/train/datasets/Advertising.csv")
+    .load("file:///tmp/Advertising.csv")
 
+  // Show 5 rows
   df.show(5)
+
+  // Stop Spark Session
+  spark.stop()
 
 }
